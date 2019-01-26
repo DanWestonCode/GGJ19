e567acbdb4c5600781337d0754e4e6f3d7e646ae
Jack Serjeant <Jack.Serjeant@gmail.com>
2019-01-26 21:45:49 +0000

    Added iTween Food Spawn

--- GGJ19/Assets/Scripts/Food.cs
+++ GGJ19/Assets/Scripts/Food.cs
@@ -9,13 +9,11 @@ public class Food : MonoBehaviour {
 	public enum PickUpStates {gotPickedUp, isPickedUp, gotDropped, Static};
 	public PickUpStates PickUpState;
 
-	// Use this for initialization
 	void Start () {
 		PickUpState = PickUpStates.Static;
 		foodManagerComponent = foodManagerGameObject.GetComponent<FoodManager>();
 	}
 
-	// Update is called once per frame
 	void Update () 
 	{
 		if(PickUpState == PickUpStates.gotPickedUp)
