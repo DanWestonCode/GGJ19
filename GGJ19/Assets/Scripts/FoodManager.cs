c23a933aa57797244588d8772b9ba86fd37508e3
Jack Serjeant <Jack.Serjeant@gmail.com>
2019-01-26 11:38:15 +0000

    trying again !!!

---
+++ GGJ19/Assets/Scripts/FoodManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour {
+
+	public int StartQuantity;
+
+	public GameObject FoodPrefab;
+
+	public GameObject[] FoodObjects; 
+
+	// Use this for initialization
+	void Start () {
+		// Spawn Food into the level
+		for (int i = 0; i < StartQuantity; i++)
+		{
+			// Create Food Object
+			Instantiate(FoodPrefab);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
