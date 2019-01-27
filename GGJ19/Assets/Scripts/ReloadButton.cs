c9c78ad211ae419d973ee5d8d923b8805a65e68c
Jack Serjeant <Jack.Serjeant@gmail.com>
2019-01-27 09:11:24 +0000

    Added reload button to scene.

---
+++ GGJ19/Assets/Scripts/ReloadButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
