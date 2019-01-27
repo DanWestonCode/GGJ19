95d58a5bf292b838dbbe9ee816f36849056e4ed3
CAMPUS\TSTD11605 <sam@mobilepie>
2019-01-27 15:52:19 +0000

    reset speed

--- GGJ19/Assets/Scripts/NodeMovement.cs
+++ GGJ19/Assets/Scripts/NodeMovement.cs
@@ -335,7 +335,7 @@ public class NodeMovement : MonoBehaviour {
             //move towards the top node of the current path
             direction = Vector3.Normalize(currentPath[0].gameObject.transform.position - gameObject.transform.position);
 
-            gameObject.transform.position += (direction * Time.deltaTime * 20);
+            gameObject.transform.position += (direction * Time.deltaTime);
         }
     }
 
